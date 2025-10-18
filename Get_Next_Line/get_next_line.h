a9049552a96b1ec8636cd7c33d2f0b3425661638
szaaza <zanjani.sasan1988@gmail.com>
2025-10-18 21:46:24 +0000

    make begin_game.c

---
+++ Get_Next_Line/get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/18 21:19:48 by codespace         #+#    #+#             */
+/*   Updated: 2025/10/18 21:19:50 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 64
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_append(char *s1, char *s2, size_t len);
+
+#endif
--- begin_game.c
+++ begin_game.c
@@ -1,36 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   begin_game.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/14 16:35:22 by codespace         #+#    #+#             */
-/*   Updated: 2025/10/18 21:39:36 by codespace        ###   ########.fr       */
+/*   Created: 2025/10/17 21:46:10 by codespace         #+#    #+#             */
+/*   Updated: 2025/10/18 21:43:38 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	file_check(char *input)
-{
-	int	len;
 
-	len = ft_strlen(input);
-	if (len < 4)
-		error_exit("Not valid name fir input");
-	if (ft_strcmp(&input[len - 4], ".ber") != 0)
-		error_exit(".ber input is required");
+static void	setup_map(t_game *game, char *input)
+{
+	file_check(input);
+	game->map.grid = load_map_from_file(input);
+	validate_map(game);
 }
 
 
-
-int	main(int argc, char **argv)
+void	begin_game(t_game *game, char *input)
 {
-	t_game	game;
-
-	if (argc != 2)
-		error_exit("Usage: ./so_long <map.ber>");
-	begin_game(&game, argv[1]);
-	return (0);
+	setup_map(game, input);
 }
